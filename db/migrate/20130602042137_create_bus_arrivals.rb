1e0aff950e5afed598783918dbaf084d2740b3ea
mildmojo <theaterfire@yahoo.com>
2013-06-02 14:03:43 -0400

    Fixes column name in arrivals table.

--- db/migrate/20130602042137_create_bus_arrivals.rb
+++ db/migrate/20130602042137_create_bus_arrivals.rb
@@ -1,7 +1,7 @@
 class CreateBusArrivals < ActiveRecord::Migration
   def change
     create_table :bus_arrivals do |t|
-      t.integer :bus_num
+      t.integer :bus_id
       t.integer :stop_id
       t.datetime :scheduled_at
       t.datetime :estimated_at
