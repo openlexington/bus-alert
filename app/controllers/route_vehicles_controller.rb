87f96dd6aa35e867205133561fdd0743ed30a281
Joe Kratzat <joe.kratzat@gmail.com>
2013-08-07 10:02:10 -0400

    Cleaned up comments in route_vehicles_controller

--- app/controllers/route_vehicles_controller.rb
+++ app/controllers/route_vehicles_controller.rb
@@ -11,14 +11,14 @@ class RouteVehiclesController < ApplicationController
   private
 
   def refresh_vehicles( route_id )
-    new_vehicles = Lextran::Vehicle.for_route(route_id)#.uniq(&:route_num)
+    new_vehicles = Lextran::Vehicle.for_route(route_id)
     if new_vehicles.present?
       RouteVehicle.transaction do
         RouteVehicle.delete_all(bus_route_id: route_id)
         new_vehicles.each do |vehicle|
           RouteVehicle.create!(vehicle.to_h)
         end
-      end # BusRoute.transaction
+      end # RouteVehicle.transaction
     end
   end # refresh_routes
 end
